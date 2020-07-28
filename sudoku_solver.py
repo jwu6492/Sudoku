d6a781139afcca1ad4796612fa555ad8f71d5669
jwu6492 <yiwu1432@gmail.com>
2020-07-28 01:17:56 -0400

    finished backtracking algorithm for sudoku board

---
+++ sudoku_solver.py
@@ -0,0 +1,66 @@
+#sudoku solver using the backtracking algorithm assuming 9x9 board
+def valid(board, row, col, num):
+    #check row
+    for i in range(0, len(board)):
+        if board[row][i] == num:
+            return False
+    #check column
+    for i in range(0, len(board)):
+        if board[i][col] == num:
+            return False
+    #check sub-grid
+    row_start = row - row % 3
+    col_start = col - col % 3
+    for i in range(row_start, row_start + 3):
+        for j in range(col_start, col_start + 3):
+            if board[i][j] == num:
+                return False
+    #valid attempted number
+    return True
+
+def find_empty_space(board, pos):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                pos[0] = i
+                pos[1] = j
+                return True
+    return False
+def solve(board):
+    pos = [0,0]
+    if not find_empty_space(board, pos):
+        return True
+    row = pos[0]
+    col = pos[1]
+    for num in range(1, 10):
+        if valid(board, row, col, num):
+            board[row][col] = num
+            if solve(board):
+                return True
+            else:
+                board[row][col] = 0
+    #no solution
+    return False
+def print_board(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            print(board[i][j], end = " ")
+        print()
+
+grid = [
+    [3, 0, 6, 5, 0, 8, 4, 0, 0],
+    [5, 2, 0, 0, 0, 0, 0, 0, 0],
+    [0, 8, 7, 0, 0, 0, 0, 3, 1],
+    [0, 0, 3, 0, 1, 0, 0, 8, 0],
+    [9, 0, 0, 8, 6, 3, 0, 0, 5],
+    [0, 5, 0, 0, 9, 0, 6, 0, 0],
+    [1, 3, 0, 0, 0, 0, 2, 5, 0],
+    [0, 0, 0, 0, 0, 0, 0, 7, 4],
+    [0, 0, 5, 2, 0, 6, 3, 0, 0]
+]
+if solve(grid):
+    print("The solved board will look like:")
+    print()
+    print_board(grid)
+else:
+    print("error")
